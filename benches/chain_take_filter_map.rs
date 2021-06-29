9b6bd7cdfbddae0109049c589ba7d143771167eb
andreas <wass.andreas@gmail.com>
2021-06-29 12:56:15 +0200

    Add handwritten benchmarks to...
    chain take filter map and
    take filter map

--- benches/chain_take_filter_map.rs
+++ benches/chain_take_filter_map.rs
@@ -23,6 +23,21 @@ fn run_generator(data: &Vec<i32>) {
     black_box(result);
 }
 
+fn run_handwritten(data: &Vec<i32>) {
+    let mut result = 0i32;
+    for x in data {
+        if x % 2 == 0 {
+            result = result.wrapping_add(x*3);
+        }
+    }
+    for x in &data[0..data.len()/2] {
+        if x % 2 == 0 {
+            result = result.wrapping_add(x*3);
+        }
+    }
+    black_box(result);
+}
+
 pub fn make_data(amount: usize) -> Vec<i32> {
     let mut retval = Vec::new();
     retval.reserve(amount);
@@ -39,6 +54,9 @@ pub fn benchmarks(c: &mut Criterion) {
     });
     c.bench_function("generator_chain_take_filter_map", |b| {
         b.iter(|| run_generator(black_box(&data)))
+    });
+    c.bench_function("handwritten_chain_take_filter_map", |b| {
+        b.iter(|| run_handwritten(black_box(&data)))
     });
 }
 
--- benches/take_filter_map.rs
+++ benches/take_filter_map.rs
@@ -21,6 +21,16 @@ fn run_generator(data: &Vec<i32>) {
     black_box(result);
 }
 
+fn run_handwritten(data: &Vec<i32>) {
+    let mut result = 0i32;
+    for x in &data[0..data.len()/2] {
+        if x % 2 == 0 {
+            result = result.wrapping_add(x*3);
+        }
+    }
+    black_box(result);
+}
+
 pub fn make_data(amount: usize) -> Vec<i32> {
     let mut retval = Vec::new();
     retval.reserve(amount);
@@ -34,6 +44,7 @@ pub fn benchmarks(c: &mut Criterion) {
     let data = make_data(1000_000);
     c.bench_function("iterator_take_filter_map", |b| b.iter(|| run_iterator(black_box(&data))));
     c.bench_function("generator_take_filter_map", |b| b.iter(|| run_generator(black_box(&data))));
+    c.bench_function("handwritten_take_filter_map", |b| b.iter(|| run_handwritten(black_box(&data))));
 }
 
 criterion_group!(benches, benchmarks);
