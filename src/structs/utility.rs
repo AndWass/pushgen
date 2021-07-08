2a8e5c787823415f6bea1f003329bbcb8a2d8f88
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-08 17:33:19 +1000

    Add new struct utility::InplaceUpdatable
    Implements associated methods for safely replacing a &mut value without
    any unsafe fn and doesn't pose any UB under panic unwind.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/structs/utility.rs
+++ src/structs/utility.rs
@@ -1,6 +1,5 @@
 use core::hint;
 
-#[allow(dead_code)]
 #[inline(always)]
 pub unsafe fn unwrap_unchecked<T>(option: Option<T>) -> T {
     match option {
@@ -8,5 +7,34 @@ pub unsafe fn unwrap_unchecked<T>(option: Option<T>) -> T {
         // unlike unreachable!() which will panic on reaching, unreachable_unchecked
         // has no effect on the generated code and it will be UB if it is actually reached.
         None => hint::unreachable_unchecked(),
+    }
+}
+
+/// Implements this functionality as a struct instead of a function taking Option<T>
+/// to make sure that we won't accidentally treats a None as a Some and avoids the
+/// unsafe fn altogether.
+pub struct InplaceUpdatable<T> {
+    // Using Option here is necessary for integrity during unwind,
+    // which is the only moment where inner is set to None.
+    //
+    // In every other cases, it is set to `Some(value)`.
+    inner: Option<T>,
+}
+impl<T> InplaceUpdatable<T> {
+    #[inline(always)]
+    pub const fn new(value: T) -> InplaceUpdatable<T> {
+        InplaceUpdatable { inner: Some(value) }
+    }
+
+    #[inline(always)]
+    pub fn update(&mut self, updater: impl FnOnce(T) -> T) {
+        // take self.inner to ensure nothing will be dropped here during unwind,
+        // if updater ever panic
+        let new_val = updater(unsafe { unwrap_unchecked(self.inner.take()) });
+        self.inner = Some(new_val);
+    }
+
+    pub fn get_inner(self) -> T {
+        unsafe { unwrap_unchecked(self.inner) }
     }
 }
