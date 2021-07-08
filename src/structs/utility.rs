db5413fcf7f6e538d4107d518924e340d76fe6d3
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-08 15:40:25 +1000

    Disable dead code warning for unwrap_unchecked to please clippy
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/structs/utility.rs
+++ src/structs/utility.rs
@@ -2,6 +2,7 @@
 // has no effect on the generated code and it will be UB if it is actually reached.
 use std::hint::unreachable_unchecked;
 
+#[allow(dead_code)]
 pub unsafe fn unwrap_unchecked<T>(option: Option<T>) -> T {
     match option {
         Some(val) => val,
