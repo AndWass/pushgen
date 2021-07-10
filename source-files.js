b11a91e04a97f00b06e372edc4538b56cbd4db3e
AndWass <AndWass@users.noreply.github.com>
2021-07-10 08:22:24 +0000

    deploy: 04c0e5bf91d5319776bf5a7faeff84b8059da150

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["pushgen"] = {"name":"","dirs":[{"name":"structs","files":["boxed.rs","chain.rs","cloned.rs","dedup.rs","filter.rs","filter_map.rs","flatten.rs","from_fn.rs","from_iter.rs","iterator.rs","map.rs","mod.rs","option.rs","skip.rs","take.rs","utility.rs","zip.rs"]}],"files":["dyn_generator.rs","generator_ext.rs","into_gen.rs","lib.rs"]};
+createSourceSidebar();
