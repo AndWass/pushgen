618446218420a7106d3fa658fafb36e79620f75b
AndWass <AndWass@users.noreply.github.com>
2021-07-20 08:27:08 +0000

    deploy: 50140ffc4a32230b962dc5a1f35560d0a23e41b5

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["pushgen"] = {"name":"","dirs":[{"name":"structs","files":["boxed.rs","chain.rs","cloned.rs","copied.rs","dedup.rs","filter.rs","filter_map.rs","flatten.rs","from_fn.rs","from_iter.rs","iterator.rs","map.rs","mod.rs","option.rs","skip.rs","take.rs","utility.rs","zip.rs"]},{"name":"traits","files":["accum.rs","mod.rs"]}],"files":["dyn_generator.rs","generator_ext.rs","into_gen.rs","lib.rs"]};
+sourcesIndex["pushgen"] = {"name":"","dirs":[{"name":"generators","files":["boxed.rs","from_fn.rs","from_iter.rs","mod.rs","option.rs","slice_generator.rs"]},{"name":"structs","files":["chain.rs","cloned.rs","copied.rs","dedup.rs","filter.rs","filter_map.rs","flatten.rs","iterator.rs","map.rs","mod.rs","skip.rs","take.rs","utility.rs","zip.rs"]},{"name":"traits","files":["accum.rs","dyn_generator.rs","generator.rs","generator_ext.rs","into_gen.rs","mod.rs"]}],"files":["lib.rs","result.rs"]};
 createSourceSidebar();
